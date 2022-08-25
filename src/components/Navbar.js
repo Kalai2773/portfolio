bd1669c2f93fc985010b002409e8c5e1c7cdad3c
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2022-08-25 23:00:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 //import logo from "../Assets/logo1.png";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
+//import { CgGitFork } from "react-icons/cg";
 //import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
+  //AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
+//import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -102,7 +102,7 @@ function NavBar() {
               </Nav.Link>
         </Nav.Item>*/}
 
-            <Nav.Item className="fork-btn">
+            {/*<Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/Kalai2773/portfolio"
                 target="_blank"
@@ -111,7 +111,7 @@ function NavBar() {
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
               </Button>
-            </Nav.Item>
+            </Nav.Item>*/}
           </Nav>
         </Navbar.Collapse>
       </Container>

0c21de54b76ec5c770ad3a23b0307c46d87f4ff3
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2022-10-05 14:12:33 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -107,7 +107,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/kalai_330/"
+                  href="https://twitter.com/kalai_53/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"

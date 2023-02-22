82be5b7998d00852043ff088587d008419ff70d4
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2023-02-22 19:12:42 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -75,7 +75,7 @@ function Footer() {
 </li>*/}
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/ig_kalai_3/"
+                href="https://www.instagram.com/kalz_irreplaceable/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"

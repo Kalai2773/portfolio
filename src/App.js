dd1b3290f96b48a970c5bbdadd22727c23d2fbbd
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2023-07-04 17:33:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AnimatedCursor from "react-animated-cursor";
 
 
 function App() {
@@ -44,17 +43,6 @@ function App() {
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
-        <AnimatedCursor 
-      color="255,255,255"
-      innerSize={8}
-      outerSize={35}
-      innerScale={0}
-      outerScale={1.7}//0
-      outerAlpha={0}
-      outerStyle={{
-        border: '3px solid #fff'
-      }}
-      />
       </div>
     </Router>
     

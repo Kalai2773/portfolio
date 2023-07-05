c666d0716496eedb6393e77bbae8d357a80b0e42
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2023-07-05 22:11:42 +0530

    Update Particle

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -4,51 +4,120 @@ import Particles from "react-tsparticles";
 function Particle() {
   return (
      <Particles
-      id="tsparticles"
-      params={{
-        particles: {
-          number: {
-            value: 160,
-            density: {
+          options={{
+            particles: {
+              number: {
+                value: 100,
+                density: {
+                  enable: false,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#919ba8",
+              },
+              shape: {
+                type: ["star", "circle"],
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 0.3,
+                  opacity_min: 0.3,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 2,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 4,
+                  size_min: 0.3,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: false,
+                distance: 100,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 0.3,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600,
+                },
+              },
+            },
+            fullscreen: {
               enable: true,
-              value_area: 1500,
+              zIndex: -2,
             },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: false,
+                  mode: "bubble",
+                },
+                onclick: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 200,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 250,
+                  size: 0,
+                  duration: 2,
+                  opacity: 0,
+                },
+                repulse: {
+                  distance: 300,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
             },
-          },
-        },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "push",
-            },
-          },
-          modes: {
-            push: {
-              particles_nb: 1,
-            },
-          },
-        },
-        retina_detect: true,
-      }}
-    />
+            retina_detect: true,
+          }}
+        />
   );
 }
 

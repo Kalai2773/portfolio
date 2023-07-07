cc919614dd85bbc94e9a74ea74847821f09b67cf
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2023-07-07 21:47:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 //import Resume from "./components/Resume/ResumeNew";
-import AnimatedCursor from "react-animated-cursor";
 import {
   BrowserRouter as Router,
   Route,
@@ -43,17 +42,6 @@ function App() {
          
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
-        <AnimatedCursor
-         color="255,255,255"
-          innerSize={1}
-          outerSize={35}
-          innerScale={1}
-          outerScale={1.7}
-          outerAlpha={0}
-          outerStyle={{
-          border: '3px solid #fff'
-           }}
-        />
         <Footer />
       </div>
     </Router>
